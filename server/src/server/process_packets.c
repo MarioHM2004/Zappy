cf848cffd33ebe2ab83f37b17487bd0f5e1043f3
Renzo Maggiori <renzo.maggiori@epitech.eu>
2024-05-29 12:22:18 +0200

    [mod] process packets loop

--- server/src/server/process_packets.c
+++ server/src/server/process_packets.c
@@ -22,21 +22,32 @@ char *get_cmd_from_packets(packet_list_t *packets)
 {
     char *cmd = calloc(MAX_BODY_LENGTH, sizeof(char));
     packet_node_t *tmp = NULL;
+    packet_node_t *current = NULL;
 
     if (!packets)
         return NULL;
     strcpy(cmd, packets->lh_first->packet->data);
-    LIST_FOREACH(tmp, packets, entries) {
-        if (!tmp->packet || is_packed_completed(tmp->packet))
+    current = LIST_FIRST(packets);
+    while (current != NULL) {
+        tmp = LIST_NEXT(current, entries);
+        LIST_REMOVE(current, entries);
+        if (!current->packet || is_packed_completed(current->packet))
             break;
-        cmd = safe_strcat(cmd, tmp->packet->data);
+        cmd = safe_strcat(cmd, current->packet->data);
+        destroy_packet_node(current);
+        current = tmp;
+
     }
     return cmd;
 }
 
-// client_commands_ptr(client, cmd);
-void process_client_packets(client_t *client)
+// client_commands_ptr(server, client, cmd);
+void process_client_packets(server_t *server, client_t *client)
 {
-    char *cmd = get_cmd_from_packets(client->request);
+    char *cmd = NULL;
     packet_t *packet = NULL;
+
+    while (!LIST_EMPTY(client->request)) {
+        cmd = get_cmd_from_packets(client->request);
+    }
 }
