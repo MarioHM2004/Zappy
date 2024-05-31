a1aca79d771290610c6d6d510fb9dcec4e9f7bc8
Alba Candelario <alba.candelario-matas@epitech.eu>
2024-05-31 14:58:49 +0200

    [add] ai-connection with server

--- ai/ai_manager.py
+++ ai/ai_manager.py
@@ -1,12 +1,6 @@
-##
-## EPITECH PROJECT, 2024
-## B-YEP-400-BAR-4-1-zappy-joan-pau.merida-ruiz
-## File description:
-## ai_manager
-##
-
 #!/usr/bin/env python3
 import argparse
+import socket
 
 class AIManager:
     def __init__(self):
@@ -21,4 +15,36 @@ class AIManager:
         self.start_ai(args)
 
     def start_ai(self, args):
-        print(f"Starting AI for team {args.n} on {args.h}:{args.p}")
+        print(f"-- Starting AI for team {args.n} on {args.h}:{args.p}")
+
+        client_socket = self.establish_connection(args.h, args.p, args.n)
+        running = True
+
+        if client_socket is None:
+            running = False
+            return
+
+        while running:
+            try:
+                data = client_socket.recv(1024).decode()
+                if not data:
+                    break
+                print(f"Received: {data}")
+            except Exception as e:
+                print(f"-- Error: {e}")
+                break
+
+        print("-- Connection closed")
+        client_socket.close()
+
+    def establish_connection(self, host, port, team_name):
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            client_socket.connect((host, port))
+            print(f"-- Connected to {host}:{port}")
+            init_msg = f"team_name:{team_name}n"
+            client_socket.sendall(init_msg.encode())
+            return client_socket
+        except Exception as e:
+            print(f"-- Failed to connect to server: {host}:{port}")
+            return None
