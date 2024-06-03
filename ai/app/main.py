844262301b747f71093125de09d50a142a61eed9
Alba Candelario <alba.candelario-matas@epitech.eu>
2024-06-03 11:58:03 +0200

    [mod] main

--- ai/app/main.py
+++ ai/app/main.py
@@ -3,14 +3,14 @@ from .modules.AIManager.AIManager import AIManager
 
 def main():
     print("Starting AI...")
-    if len(sys.argv) > 1 and len(sys.argv) <= 7:
-        ai_manager = AIManager()
-        try:
-            ai_manager.parse_args(sys.argv[1:])
-        except Exception as e:
-            print(e)
-            sys.exit(84)
-    else:
+    if len(sys.argv) < 2 or len(sys.argv) > 7:
         print("ERROR: bad arguments.")
         print("Try './zappy_ai -help' for more information.")
+        sys.exit(84)
+
+    ai_manager = AIManager()
+    try:
+        ai_manager.parse_args(sys.argv[1:])
+    except Exception as e:
+        print(e)
         sys.exit(84)
