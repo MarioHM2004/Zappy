b9df3549a7e542aee4effaa8fa828c06716291db
Oriol Linan <oriol.linan@epitech.eu>
2024-06-03 10:46:46 +0200

    [mod] fix error with bct command

--- server/src/server/commands/gui/bct.c
+++ server/src/server/commands/gui/bct.c
@@ -9,31 +9,36 @@
 #include "server/command.h"
 #include "game/map.h"
 #include "game/resources.h"
+#include "server/packet.h"
 
 char *get_tile_content(server_t *server, int width, int height)
 {
-    char *res = formatstr(BCT_RESPONSE, width, height,
-        (int)server->game->map->tiles[width][height]->resource->food,
-        (int)server->game->map->tiles[width][height]->resource->linemate,
-        (int)server->game->map->tiles[width][height]->resource->deraumere,
-        (int)server->game->map->tiles[width][height]->resource->sibur,
-        (int)server->game->map->tiles[width][height]->resource->mendiane,
-        (int)server->game->map->tiles[width][height]->resource->phiras,
-        (int)server->game->map->tiles[width][height]->resource->thystame,
+    return formatstr(BCT_RESPONSE, width, height,
+        (int)server->game->map->tiles[width][height].resource->food,
+        (int)server->game->map->tiles[width][height].resource->linemate,
+        (int)server->game->map->tiles[width][height].resource->deraumere,
+        (int)server->game->map->tiles[width][height].resource->sibur,
+        (int)server->game->map->tiles[width][height].resource->mendiane,
+        (int)server->game->map->tiles[width][height].resource->phiras,
+        (int)server->game->map->tiles[width][height].resource->thystame
     );
-    return res;
 }
 
 void bct_command(server_t *server, client_t *client, char *cmd)
 {
     int width;
     int height;
-    char *response;
+    char *response = NULL;
     packet_t *packet = NULL;
 
-    scanf(BCT_REQUEST, &width, &height);
-    if (server->game->map->width >= width || server->game->map->height >= height)
+    sscanf(cmd, BCT_REQUEST, &width, &height);
+    if (width >= server->game->map->width || height >= server->game->map->height)
         return packet_error(client);
     response = get_tile_content(server, width, height);
+    if (!response)
+        return packet_error(client);
     packet = create_packet(response);
+    if (!packet)
+        return packet_error(client);
+    add_packet(client->response, packet);
 }
