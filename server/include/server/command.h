46b04f10fda452db20bd8227f4ac28080378eb25
Renzo Maggiori <renzo.maggiori@epitech.eu>
2024-06-06 15:31:07 +0200

    [add] pdi command

--- server/include/server/command.h
+++ server/include/server/command.h
@@ -70,6 +70,7 @@ void sgt_command(server_t *server, client_t *client, char *cmd);
 void sst_command(server_t *server, client_t *client, char *cmd);
 void seg_command(server_t *server, client_t *client);
 void pnw_command(server_t *server, client_t *client, uint player_num);
+void pdi_command(server_t *server, client_t *client, player_t *player);
 
 char *get_tile_content(server_t *server, int width, int height);
 char *get_cmd_from_packets(packet_list_t *packets);
