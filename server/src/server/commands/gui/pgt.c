bb44174a9feb35368cfe8cc46f9d768744f87fee
Renzo Maggiori <renzo.maggiori@epitech.eu>
2024-06-06 17:17:38 +0200

    [rm] packet from pdr && pgt

--- server/src/server/commands/gui/pgt.c
+++ server/src/server/commands/gui/pgt.c
@@ -13,8 +13,6 @@
 void pgt_command(server_t *server, client_t *client,
     player_t *player, resource_e resource)
 {
-    packet_t *packet = NULL;
-
     if (!player)
         return packet_message(client, ERROR_MESSAGE);
     add_response(client, formatstr(PGT_RESPONSE, (int)player->number, resource));
