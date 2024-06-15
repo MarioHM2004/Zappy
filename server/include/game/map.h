53f1efaa8f256261fb0702b9c1e7435446d05dae
gonzalo <gonzalo.larroya-martinez@epitech.eu>
2024-06-15 13:14:52 +0200

    [add] eject command

--- server/include/game/map.h
+++ server/include/game/map.h
@@ -53,5 +53,7 @@ position_t pos_at(map_t *map, position_t pos);
 position_t dir_at(map_t *map, position_t pos, direction_e dir);
 position_t get_random_pos(map_t *map);
 direction_e get_random_dir(void);
+direction_e right_dir(direction_e dir);
+direction_e left_dir(direction_e dir);
 
 #endif /* !MAP_H_ */
