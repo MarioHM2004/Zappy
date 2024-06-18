f83eca29a1a38803fe657dab555143bd4e0b9a9b
Alba Candelario <alba.candelario-matas@epitech.eu>
2024-06-18 10:41:28 +0200

    [add] fork

--- ai/app/modules/Drone/Algorithm/Algorithm.py
+++ ai/app/modules/Drone/Algorithm/Algorithm.py
@@ -50,4 +50,4 @@ class Algorithm:
             return "ko"
         # scores = self.__calculate_scores(payload)
         # return max(scores, key=scores.get)
-        return "connect_nbr" # Testing purposes
+        return "eject" # Testing purposes
--- ai/app/modules/Drone/DroneHelper.py
+++ ai/app/modules/Drone/DroneHelper.py
@@ -17,7 +17,7 @@ if typing.TYPE_CHECKING:
 # def cmd_move(drone: Drone, payload) -> str:
 # pass
 
-
+# takes 7/f
 def cmd_forward(drone: Drone, payload) -> str:
     match drone.orientation:
         case 1:
@@ -30,7 +30,7 @@ def cmd_forward(drone: Drone, payload) -> str:
             drone.x_position -= 1
     return "forward"
 
-
+# takes 7/f
 def cmd_left(drone: Drone, payload) -> str:
     match drone.orientation:
         case 1:
@@ -43,7 +43,7 @@ def cmd_left(drone: Drone, payload) -> str:
             drone.orientation = 3
     return "left"
 
-
+# takes 7/f
 def cmd_right(drone: Drone, payload) -> str:
     match drone.orientation:
         case 1:
@@ -56,40 +56,41 @@ def cmd_right(drone: Drone, payload) -> str:
             drone.orientation = 1
     return "right"
 
-
+# takes 7/f
 def cmd_look(drone: Drone, payload) -> str:
     print(f"look: {drone.view}")
     return "look"
 
-
+# takes 1/f
 def cmd_inventory(drone: Drone, payload) -> str:
     print(f"inventory: {drone.inventory}")
     return "inventory"
 
-
+# takes 7/f
 def cmd_broadcast(drone, msg: str) -> str:
     return f"broadcast {msg}"
 
-
+# doesn't take any time
 def cmd_connect_nbr(drone: Drone, payload) -> str:
     return "connect_nbr"
 
-
+# takes 42/f
 def cmd_fork(drone: Drone, payload) -> str:
+    drone.connect_nbr += 1
     return "fork"
 
-
+# takes 7/f
 def cmd_eject(drone: Drone, payload) -> str:
     return "eject"
 
-
+# takes 7/f
 def cmd_take(drone: Drone, payload) -> str:
     return "take"
 
-
+# takes 7/f
 def cmd_set(drone: Drone, payload) -> str:
     return "set"
 
-
+# takes 300/f
 def cmd_incantation(drone: Drone, payload) -> str:
     return "incantation"
