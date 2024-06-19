4118a5bb9c64bc8802d6c5a314c35da4295e8bca
Mario Alessandro Herranz Machado <mario-alessandro.herranz-machado@epitech.eu>
2024-06-19 16:27:03 +0200

    add: updated test server for testing with new commands (#57)

--- game/source/Server.cpp
+++ game/source/Server.cpp
@@ -1,9 +1,11 @@
 #include <algorithm>
 #include <arpa/inet.h>
+#include <chrono>
 #include <cstddef>
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
+#include <format>
 #include <iostream>
 #include <netinet/in.h>
 #include <random>
@@ -12,6 +14,7 @@
 #include <sys/select.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <thread>
 #include <unistd.h>
 #include <vector>
 
@@ -59,11 +62,12 @@ void zappy::Server::newClient()
         accept(_serverSocket, (struct sockaddr *) &_clientAddr, &addrSize);
     if (newSocket < 0)
         std::cerr << "accept error\n";
-    std::cout << "A new bot has connected" << std::endl;
     FD_SET(newSocket, &_currentSockets);
     for (int i = 0; i < FD_SETSIZE; i++) {
         if (_clientSockets[i] == 0) {
             _clientSockets[i] = newSocket;
+            _initialDataSent[i] = false; // Reset initial data sent flag
+            sendInitialData(newSocket);  // Send initial data immediately
             break;
         }
     }
@@ -72,120 +76,149 @@ void zappy::Server::newClient()
 std::string zappy::Server::generate(int x, int y)
 {
     std::stringstream msg;
-    msg << "mct " << x << " " << y;
+    msg << "bct " << x << " " << y;
     std::random_device rd;
     rng.seed(rd());
-    std::uniform_int_distribution<int> dist(0, 100);
+    std::uniform_int_distribution<int> dist(0, 5);
     for (int i = 0; i < 7; ++i) {
         msg << " " << dist(rng);
     }
     return msg.str() + "\n";
 }
 
-void zappy::Server::clientInteractions()
+void zappy::Server::sendMapSize(int clientSocket)
 {
-    if (_inputs.empty()) {
-        std::cerr << "input should be not empty";
+    std::stringstream msg;
+    msg << "msz " << _x << " " << _y << std::endl;
+    std::string message = msg.str();
+    ssize_t bytesSent = send(clientSocket, message.c_str(), message.size(), 0);
+    std::cout << message.c_str();
+    if (bytesSent < 0) {
+        std::cerr << "Error sending map size to client.\n";
+        close(clientSocket);
+        FD_CLR(clientSocket, &_currentSockets);
     }
-    if (_inputs[0] == "msz") {
-        std::stringstream msg;
-        msg << "msz " << _x << " " << _y << std::endl;
-        std::string message = msg.str();
-        ssize_t bytesSent =
-            send(_clientSocket, message.c_str(), message.size(), 0);
-        if (bytesSent < 0) {
-            std::cerr << "Error sending data to client.\n";
-        }
-    }
-    if (_inputs[0] == "mct") {
-        for (int i = 0; i <= _x; i++) {
-            for (int j = 0; j <= _y; j++) {
-                std::string msg = generate(i, j);
-                ssize_t bytesSent =
-                    send(_clientSocket, msg.c_str(), msg.size(), 0);
-                if (bytesSent < 0) {
-                    std::cerr << "Error sending data to client.\n";
-                }
+}
+
+void zappy::Server::sendMapContent(int clientSocket)
+{
+    for (int i = 0; i <= _x; i++) {
+        for (int j = 0; j <= _y; j++) {
+            std::string msg = generate(i, j);
+            ssize_t bytesSent = send(clientSocket, msg.c_str(), msg.size(), 0);
+            std::cout << msg.c_str();
+            if (bytesSent < 0) {
+                std::cerr << "Error sending map content to client.\n";
+                close(clientSocket);
+                FD_CLR(clientSocket, &_currentSockets);
+                return;
             }
         }
     }
-    if (_inputs[0] == "echo") {
-        std::string message = _clientResponse + "\n";
-        message.erase(0, message.find(" ") + 1);
+}
+
+void zappy::Server::addNewPlayer(int clientSocket)
+{
+    for (int n = 0; n < playerCount; ++n) {
+        std::stringstream msg;
+        msg << "pnw " << n << " " << 0 << " " << n << " 2 1 " << n << std::endl;
+        std::string message = msg.str();
         ssize_t bytesSent =
-            send(_clientSocket, message.c_str(), message.size(), 0);
+            send(clientSocket, message.c_str(), message.size(), 0);
+        std::cout << message.c_str();
         if (bytesSent < 0) {
-            std::cerr << "Error sending data to client.\n";
+            std::cerr << "Error sending map size to client.\n";
+            close(clientSocket);
+            FD_CLR(clientSocket, &_currentSockets);
         }
     }
 }
 
-void zappy::Server::trim(std::string &s)
+void zappy::Server::initTeams(int clientSocket)
 {
-    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
-        return !std::isspace(ch);
-    }));
-    s.erase(std::find_if(s.rbegin(), s.rend(),
-                [](unsigned char ch) {
-                    return !std::isspace(ch);
-                })
-                .base(),
-        s.end());
-}
-
-void zappy::Server::responseParser()
-{
-    _inputs.clear();
-    trim(_clientResponse);
-    std::istringstream iss(_clientResponse);
-    std::string input;
-
-    while (std::getline(iss, input, ' ')) {
-        if (!input.empty())
-            _inputs.push_back(input);
-    }
-    clientInteractions();
-    _clientResponse.clear();
-}
-
-void zappy::Server::clientHandler(int bytes)
-{
-    if (bytes <= 0) {
-        if (bytes < 0)
-            std::cerr << "read failed\n";
-        close(_clientSocket);
-        FD_CLR(_clientSocket, &_currentSockets);
-    } else {
-        std::cout << "response is --> " << _clientResponse << std::endl;
-        responseParser();
+    for (int n = 0; n < playerCount; ++n) {
+        std::stringstream msg;
+        msg << "tna " << n << std::endl;
+        std::string message = msg.str();
+        ssize_t bytesSent =
+            send(clientSocket, message.c_str(), message.size(), 0);
+        std::cout << message.c_str();
+        if (bytesSent < 0) {
+            std::cerr << "Error sending map size to client.\n";
+            close(clientSocket);
+            FD_CLR(clientSocket, &_currentSockets);
+        }
     }
 }
 
-void zappy::Server::clientConnection(int clientSocket)
+void zappy::Server::sendWelcome(int clientSocket)
 {
-    int bytes = 0;
-    char buffer[1024];
-
-    _clientSocket = clientSocket;
-    _clientResponse.clear();
-    bytes = read(_clientSocket, buffer, sizeof(buffer));
-    if (bytes < 0) {
-        std::cerr << "Error reading from client socket.\n";
-        close(_clientSocket);
-        return;
+    std::stringstream msg;
+    msg << "WELCOME" << std::endl;
+    std::string message = msg.str();
+    ssize_t bytesSent = send(clientSocket, message.c_str(), message.size(), 0);
+    std::cout << message.c_str();
+    if (bytesSent < 0) {
+        std::cerr << "Error sending map size to client.\n";
+        close(clientSocket);
+        FD_CLR(clientSocket, &_currentSockets);
     }
-    _clientResponse += std::string(buffer, bytes);
-    clientHandler(bytes);
 }
 
-void zappy::Server::clientActivity()
+void zappy::Server::movePlayer(int clientSocket)
 {
-    int clientSocket = 0;
+    for (int n = 0; n < playerCount; ++n) {
+        std::stringstream msg;
+        msg << "ppo " << n << " " << _x -1 - n << " " << _y - 1 - n << " " << 0 << std::endl;
+        std::string message = msg.str();
+        ssize_t bytesSent =
+            send(clientSocket, message.c_str(), message.size(), 0);
+        std::cout << message.c_str();
+        if (bytesSent < 0) {
+            std::cerr << "Error sending map size to client.\n";
+            close(clientSocket);
+            FD_CLR(clientSocket, &_currentSockets);
+        }
+    }
+}
 
+void zappy::Server::sendInitialData(int clientSocket)
+{
+    sendWelcome(clientSocket);
+    sendMapSize(clientSocket);
+    // sendMapContent(clientSocket);
+    initTeams(clientSocket);
+    addNewPlayer(clientSocket);
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    movePlayer(clientSocket);
+    // move to
+
+    // 0, 0
+    // 0, 1
+
+    // _x - 1 - n, 0
+    // _y - 1 - n, 0
+}
+
+void zappy::Server::sendOtherData(int clientSocket)
+{
+    // Example: Send some other message or data
+    // std::string msg = "other_data " + std::to_string(rand() % 100) + "\n";
+    // ssize_t bytesSent = send(clientSocket, msg.c_str(), msg.size(), 0);
+    // if (bytesSent < 0) {
+    //    std::cerr << "Error sending other data to client.\n";
+    //    close(clientSocket);
+    //    FD_CLR(clientSocket, &_currentSockets);
+    //}
+}
+
+void zappy::Server::sendPeriodicData()
+{
     for (int i = 0; i < FD_SETSIZE; i++) {
-        clientSocket = _clientSockets[i];
-        if (clientSocket > 0 && FD_ISSET(clientSocket, &_readySockets))
-            clientConnection(clientSocket);
+        int clientSocket = _clientSockets[i];
+        if (clientSocket != 0 && _initialDataSent[i]) {
+            sendOtherData(clientSocket);
+        }
     }
 }
 
@@ -193,17 +226,25 @@ void zappy::Server::runServer()
 {
     FD_ZERO(&_currentSockets);
     FD_SET(_serverSocket, &_currentSockets);
-    for (int i = 0; i < FD_SETSIZE; i++)
+    for (int i = 0; i < FD_SETSIZE; i++) {
         _clientSockets[i] = 0;
+        _initialDataSent[i] = false; // Initialize initial data sent flag
+    }
+
     while (1) {
         _readySockets = _currentSockets;
         if (select(FD_SETSIZE, &_readySockets, NULL, NULL, NULL) < 0)
             std::cerr << "select error";
+
         if (FD_ISSET(_serverSocket, &_readySockets))
             newClient();
-        clientActivity();
+
+        sendPeriodicData();
+        std::this_thread::sleep_for(
+            std::chrono::seconds(5)); // Adjust interval as needed
     }
 }
+
 int main(int ac, char **av)
 {
     zappy::Server server;
