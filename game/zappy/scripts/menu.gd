eaa1783db97953b2e81cbd374f37f3c27a0d4353
MarioHM2004 <mario-alessandro.herranz-machado@epitech.eu>
2024-06-21 17:07:06 +0200

    delete: unnecesary code

--- game/zappy/scripts/menu.gd
+++ game/zappy/scripts/menu.gd
@@ -1,20 +1,10 @@
 extends Control
 
-signal server_address(address: String, port: int);
-
-@onready var StartBtn = $MarginContainer/VBoxContainer/StartBtn
-@onready var QuitBtn = $MarginContainer/VBoxContainer/QuitBtn
-
-
 func _ready():
 	pass
 
 func _on_start_btn_pressed() -> void:
-	var server_ip: LineEdit = get_node("MarginContainer/VBoxContainer/ServerIp")
-	var server_port: LineEdit = get_node("MarginContainer/VBoxContainer/ServerPort")
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
-	# TODO: set ip and port in code
-	# TODO: check server is up 
-	
+
 func _on_quit_btn_pressed() -> void:
 	get_tree().quit()
